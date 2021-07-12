9db2fab2fc091f284cc8f0bfc0c7327726bd09cd
Shubhendra Kushwaha <shubhendrakushwaha94@gmail.com>
2021-07-12 16:51:36 +0530

    Add http request handler

---
+++ webhook-server/__main__.py
@@ -0,0 +1,12 @@
+import asyncio
+from server import serve
+import logging
+
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(message)s",
+    level=logging.INFO,
+)
+
+loop = asyncio.get_event_loop()
+loop.create_task(serve())
+loop.run_forever()
---
+++ webhook-server/server.py
@@ -0,0 +1,26 @@
+import asyncio
+from aiohttp import web
+from mapper import register_webhook
+
+
+async def handler(request):
+    if request.path == "/registerWebhook":
+        data = await request.json()
+        register_webhook (data["webhook_url"], data["username"])
+    return web.Response(text="OK")
+
+
+async def serve():
+    server = web.Server(handler)
+    runner = web.ServerRunner(server)
+    await runner.setup()
+    site = web.TCPSite(runner, "localhost", 8080)
+    await site.start()
+
+    print("======= Serving on http://127.0.0.1:8080/ ======")
+
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.create_task(serve())
+    loop.run_forever()
